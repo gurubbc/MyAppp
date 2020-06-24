bb2a6be8f9ecca0093049b8d1b6495ae5ae464ba
Guru <gurumurthy.ramamurthy@gmail.com>
2020-06-24 11:24:44 +0530

    Adding TestCalculator.java

---
+++ MyAppp/src/main/java/com/ofss/MyAppp/TestCalculator.java
@@ -0,0 +1,11 @@
+package com.ofss.MyAppp;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		System.out.println(c.add(545, 55));
+
+	}
+
+}
